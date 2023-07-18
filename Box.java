74ac7f98ddc2fec769f463b04080c0aa3262e406
drkop <satanatyn@yandex.ru>
2023-07-18 19:49:54 +0300

    hwoop3

---
+++ Box.java
@@ -0,0 +1,29 @@
+
+public class Box {
+
+    private int idBox;
+    private int weightBox;
+    public Box(int idBox, int weightBox) {
+        this.idBox = idBox;
+        this.weightBox = weightBox;
+    }
+
+    @Override
+    public String toString() {
+        return "Box: " +
+                "idBox=" + idBox +
+                " weightBox=" + weightBox;
+    }
+
+    public int getIdBox() {
+        return idBox;
+    }
+
+    public int getWeightBox() {
+        return weightBox;
+    }
+
+    public void setWeightBox(int weightBox) {
+        this.weightBox = weightBox;
+    }
+}
